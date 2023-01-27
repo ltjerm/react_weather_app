e3c1199d61a0e61b1c83ad19a3922c25cd8d1054
Jeremy Hernandez <jeremyhernandez@Jeremys-MacStudio.local>
2023-01-26 17:42:56 -0700

    weather appp

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '508a79394dmsh0ec344fefc22975p139222jsn03d50f27746b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
